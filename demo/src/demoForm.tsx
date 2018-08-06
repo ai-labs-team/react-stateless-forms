357055067dc317b0e525b57b7d32ce8dbd1f85b1
Daniel Hawthorn <danielh@advisoril.com>
2018-08-06 13:00:17 -0400

    Simplify demo form

--- demo/src/demoForm.tsx
+++ demo/src/demoForm.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 
-import { withProps } from 'casium';
-
 import {
   IsRequiredValidator,
   ValidatedForm,
@@ -10,45 +8,25 @@ import {
   ValidationSet,
 } from 'casium-forms';
 
-export type Props = {
-  fieldValues: object;
-  errors: [any];
-  onSubmit: (d?: any) => void;
-  onUpdate: (d?: any) => void;
-};
+const demoValidationSet = new ValidationSet({
+  requiredInput: [new IsRequiredValidator()],
+});
 
-type ComputedProps = {
-  validationSet: ValidationSet;
-};
-
-const DemoForm: any = withProps<Props, ComputedProps>(
-  {
-    validationSet: () => new ValidationSet({
-      requiredInput: [new IsRequiredValidator()],
-    })
-  },
-  ({ validationSet, errors, onSubmit, onUpdate, fieldValues }) => (
-    <ValidatedForm
-      errors={errors}
-      validationSet={validationSet}
-      onSubmit={onSubmit}
-      onUpdate={onUpdate}
-      fieldValues={fieldValues}
-    >
-      <ValidatedTextInput
-        key='requiredInput'
-        name='requiredInput'
-        type='text'
-        label='Required Text Input'
+const DemoForm = () => (
+  <ValidatedForm validationSet={demoValidationSet}>
+    <ValidatedTextInput
+      key='requiredInput'
+      name='requiredInput'
+      type='text'
+      label='Required Text Input'
+    />
+    <div className='controls'>
+      <ValidatedSubmitButton
+        key='submitBtn'
+        value='Submit'
       />
-      <div className='controls'>
-        <ValidatedSubmitButton
-          key='submitBtn'
-          value='Submit'
-        />
-      </div>
-    </ValidatedForm>
-  )
+    </div>
+  </ValidatedForm>
 );
 
 export default DemoForm;
