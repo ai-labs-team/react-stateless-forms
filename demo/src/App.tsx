ec2ad8fcaa1d7d4eeb7e0f71b8839c99b2a9e74c
Daniel Hawthorn <danielh@advisoril.com>
2018-08-06 16:14:20 -0400

    Refactor and add match validation example

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import './App.css';
 
-import DemoForm from './demoForm';
+import MatchValidationExample from './matchValidationExample';
+import RequiredInputsExample from './requiredInputsExample';
 
 class App extends React.Component {
 
@@ -11,8 +12,13 @@ class App extends React.Component {
         <header className='App-header'>
           <h1 className='App-title'>casium-forms demo</h1>
         </header>
+
         <div className='App-intro'>
-          <DemoForm />
+          <h3>Example: One optional input, one required</h3>
+          <RequiredInputsExample />
+
+          <h3>Example: Ensure field values match</h3>
+          <MatchValidationExample />
         </div>
       </div>
     );
---
+++ demo/src/matchValidationExample.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+import {
+  FieldMatchMetavalidator,
+  IsRequiredValidator,
+  ValidatedForm,
+  ValidatedSubmitButton,
+  ValidatedTextInput,
+  ValidationSet,
+} from 'casium-forms';
+
+const requiredInputsExample = () => {
+  const validationSet = new ValidationSet({
+    matchingField: [new IsRequiredValidator(), new FieldMatchMetavalidator('field')],
+  });
+
+  return (
+    <ValidatedForm validationSet={validationSet}>
+      <ValidatedTextInput name='field' label='A Field'/>
+      <ValidatedTextInput name='matchingField' label='This Field Must Match'/>
+      <ValidatedSubmitButton key='submitBtn' value='Submit'/>
+    </ValidatedForm>
+  );
+};
+
+export default requiredInputsExample as React.SFC;
