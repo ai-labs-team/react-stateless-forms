42c1a144c1e39680ba08e5dcd33cb30e82ea792a
Daniel Hawthorn <danielh@advisoril.com>
2018-09-06 13:00:14 -0400

    Start styling things

--- src/inputs/formatted_input/index.tsx
+++ src/inputs/formatted_input/index.tsx
@@ -111,7 +111,7 @@ export default class FormattedInput extends React.Component<FormattedInputProps,
     const value = this.state.formattedValue, tabIndex = disableTab ? -1 : 0;
 
     return (
-      <section className={`FormattedInput input-style  ${error ? 'error' : ''}`}>
+      <section className={`FormattedInput stateless-forms-input-style  ${error ? 'error' : ''}`}>
         <input
           ref={this.setInputRef}
           data-test-id={name}
